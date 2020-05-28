d574b3d5a8105a6a6132d652a3760cd747c5ac3d
viandwi24 <fiandwi0424@gmail.com>
2020-05-28 19:01:24 +0700

    -

--- support/config.ts
+++ support/config.ts
@@ -5,6 +5,7 @@ export class Config {
 
     async load() {
         let path = app().make("denova.path") + "/config";
+        console.log(path);
         for (const dir of Deno.readDirSync(path)) {
             let config = await import(path + "/" + dir.name);
             this.configs[dir.name.replace(".ts", "")] = config.default;
