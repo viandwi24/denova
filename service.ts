64aee4d9064164abfbe0d90263833bc493845c95
viandwi24 <fiandwi0424@gmail.com>
2020-06-01 20:26:47 +0700

    update example

--- service.ts
+++ service.ts
@@ -1,3 +1,5 @@
+// Copyright 2020 the Denova authors. All rights reserved. MIT license.
+
 import { DI } from "./deps.ts";
 
 export const Service = DI.Service;
--- version.ts
+++ version.ts
@@ -1,1 +1,3 @@
+// Copyright 2020 the Denova authors. All rights reserved. MIT license.
+
 export const version: string = "0.1.3";
