24b354a6756baeed132e43b3262c43b20c83edcc
viandwi24 <fiandwi0424@gmail.com>
2020-06-02 07:42:33 +0700

    update example

--- example/template-like-laravel/routes/web.ts
+++ example/template-like-laravel/routes/web.ts
@@ -3,3 +3,8 @@ import { Router, view, version } from "../mod.ts";
 Router.get('/', async () => {
     return await view('welcome', { name: 'World', version });
 });
+
+Router.get('/example', 'ExampleController@index');
+Router.post('/example', 'ExampleController@post');
+Router.get('/example/api', 'ExampleController@api');
+Router.get('/example/:id', 'ExampleController@show');
